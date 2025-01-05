6c90d3fc7d0698a81bf5a1112e85c662b37c649a
joncaudill <jon.caudill@outlook.com>
2025-01-04 22:08:46 -0500

    added map and mapb functionality

--- main.go
+++ main.go
@@ -2,10 +2,37 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
 )
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+type pokeAreas struct {
+	Count    int     `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		Url  string `json:"url"`
+	} `json:"results"`
+}
+
+type config struct {
+	nextUrl *string
+	prevUrl *string
+}
+
+// need to have closure on this variable since it is used later, but not defined yet
+var validCommands map[string]cliCommand
+var curIndexUrls config
 
 func cleanInput(text string) []string {
 	text = strings.ToLower(text)
@@ -13,13 +40,169 @@ func cleanInput(text string) []string {
 	return splitStrings
 }
 
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandMap() error {
+	baseUrl := ""
+	if curIndexUrls.nextUrl == nil && curIndexUrls.prevUrl == nil {
+		baseUrl = "https://pokeapi.co/api/v2/location/"
+	}
+
+	if curIndexUrls.nextUrl == nil && curIndexUrls.prevUrl != nil {
+		fmt.Println("No more areas to display")
+		return nil
+	}
+
+	if curIndexUrls.nextUrl != nil {
+		baseUrl = *curIndexUrls.nextUrl
+	}
+
+	//create request
+	req, err := http.NewRequest("GET", baseUrl, nil)
+	if err != nil {
+		return err
+	}
+	//create header
+
+	//make client and send request
+	client := &http.Client{}
+	res, err := client.Do(req)
+
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	//parse response
+	var pokedexAreas pokeAreas
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&pokedexAreas)
+	if err != nil {
+		return err
+	}
+
+	curIndexUrls.nextUrl = pokedexAreas.Next
+	curIndexUrls.prevUrl = pokedexAreas.Previous
+
+	fmt.Println()
+	for _, area := range pokedexAreas.Results {
+		fmt.Println(area.Name)
+	}
+	fmt.Println()
+
+	return nil
+}
+
+func commandMapb() error {
+	baseUrl := ""
+	if curIndexUrls.nextUrl == nil && curIndexUrls.prevUrl == nil {
+		baseUrl = "https://pokeapi.co/api/v2/location/"
+	}
+
+	if curIndexUrls.prevUrl == nil && curIndexUrls.nextUrl != nil {
+		fmt.Println("No more areas to display")
+		return nil
+	}
+
+	if curIndexUrls.prevUrl != nil {
+		baseUrl = *curIndexUrls.prevUrl
+	}
+
+	//create request
+	req, err := http.NewRequest("GET", baseUrl, nil)
+	if err != nil {
+		return err
+	}
+	//create header
+
+	//make client and send request
+	client := &http.Client{}
+	res, err := client.Do(req)
+
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	//parse response
+	var pokedexAreas pokeAreas
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&pokedexAreas)
+	if err != nil {
+		return err
+	}
+
+	curIndexUrls.nextUrl = pokedexAreas.Next
+	curIndexUrls.prevUrl = pokedexAreas.Previous
+
+	fmt.Println()
+	for _, area := range pokedexAreas.Results {
+		fmt.Println(area.Name)
+	}
+	fmt.Println()
+
+	return nil
+}
+
+func commandHelp() error {
+
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, cmdData := range validCommands {
+		fmt.Printf("%s: %s\n", cmdData.name, cmdData.description)
+	}
+	return nil
+}
+
 func main() {
+	curIndexUrls = config{}
+
+	validCommands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Displays the next 20 areas in the pokedex",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays the previous 20 areas in the pokedex",
+			callback:    commandMapb,
+		},
+	}
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex >")
 		scanner.Scan()
 		rawInput := scanner.Text()
 		input := cleanInput(rawInput)
-		fmt.Printf("Your command was: %s\n", input[0])
+		command := input[0]
+		//fmt.Printf("Your command was: %s\n", command[0])
+		cmdData, exists := validCommands[command]
+		if !exists {
+			fmt.Printf("Unknown command")
+			continue
+		}
+		//indexUrls := &config{}
+		err := cmdData.callback()
+		if err != nil {
+			fmt.Println("Error executing command: ", err)
+		}
+
+		fmt.Println()
 	}
 }
